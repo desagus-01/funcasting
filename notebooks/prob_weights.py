c669148d31009c2cff088a7ee5aeefbdbe3b2571
desagus <gugafera2016@outlook.com>
2025-09-11 13:30:05 +0100

    adding crisp probabilities

--- notebooks/prob_weights.py
+++ notebooks/prob_weights.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from get_data import get_example_assets
-from maths.non_parametric import exp_decay_probs
+from maths.non_parametric import exp_decay_probs, time_crisp_window
 
 tickers = ["AAPPL", "MSFT", "GOOG"]
 
@@ -9,6 +9,8 @@ assets = get_example_assets(tickers)
 
 increms_df = assets.increments
 
-scenario_probs = exp_decay_probs(increms_df, 50)
+exp_dec_probs = exp_decay_probs(increms_df, 50)
 
-print(scenario_probs.ndim)
+time_probs = time_crisp_window(increms_df, len(increms_df))
+
+print(time_probs)
--- src/maths/non_parametric.py
+++ src/maths/non_parametric.py
@@ -2,23 +2,29 @@ from typing import Sized
 
 import numpy as np
 from numpy.typing import NDArray
-from polars import DataFrame
 
 
-def exp_decay_probs(vector: Sized | DataFrame, half_life: int) -> NDArray[np.float64]:
+def exp_decay_probs(vector: Sized, half_life: int) -> NDArray[np.float64]:
     """
-    Return a length-n vector of recency-weighted probabilities with exponential decay.
+    Returns probability vector with exponential decay.
 
     This allows us to bake in recency bias to our otherwise uniform prior.
     """
     n = len(vector)
-
     n_array = np.arange(n)
-
     decay_rate = float(np.log(2) / half_life)
-
     latest_date = n - 1
 
     weights: NDArray[np.float64] = np.exp(-decay_rate * (latest_date - n_array))
 
     return weights / np.sum(weights)  # standardise to ensure probs
+
+
+def time_crisp_window(vector: Sized, window: int) -> NDArray[np.float64]:
+    """
+    Returns a probability vector based on the window chosen.
+    """
+    n = len(vector)
+    p = np.zeros(n, dtype=np.float64)
+    p[-window:] = 1.0 / window
+    return p
