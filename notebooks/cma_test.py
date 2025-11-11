12738841b84934f67d57cdf3875cd6b1d3b2d8b8
desagus <gugafera2016@outlook.com>
2025-11-11 16:11:13 +0000

    adding funcs for cma sep

--- notebooks/cma_test.py
+++ notebooks/cma_test.py
@@ -1,5 +1,6 @@
 import polars as pl
 
+from cma.operations import cma_separation
 from flex_probs.prob_vectors import uniform_probs
 from template import test_template
 
@@ -26,33 +27,4 @@ test = (
 
 multi_df = info["increms_df"]
 
-cdf_cols = {}
-copula_cols = {}
-
-for col in multi_df.iter_columns():
-    name = col.name
-    df = pl.DataFrame({name: col})
-    n = df.height
-    norm_const = n / (n + 1)
-    prob = uniform_probs(n)
-
-    temp = (
-        df.with_row_index()
-        .with_columns(prob=prob)
-        .sort(name)
-        .with_columns(
-            cdf=pl.cum_sum("prob") * norm_const,
-        )
-        .with_columns(
-            pobs=pl.col("cdf").gather(pl.col("index").arg_sort()),
-        )
-    )
-
-    # you said you’ll just pluck pobs, so do that
-    cdf_cols[name] = temp["cdf"]
-    copula_cols[name] = temp["pobs"]
-
-cdf = pl.DataFrame(cdf_cols)
-copula = pl.DataFrame(copula_cols)
-
-print(copula)
+print(cma_separation(multi_df, prob))
--- src/cma/operations.py
+++ src/cma/operations.py
@@ -1,17 +1,38 @@
-import numpy as np
 import polars as pl
-import polars.selectors as cs
-from numpy.typing import NDArray
-from polars._typing import RankMethod
+
+from data_types.vectors import ProbVector
 
 
-def pseudo_observations(
-    data: pl.DataFrame, method: RankMethod = "average"
-) -> pl.DataFrame | NDArray[np.floating]:
-    return data.select(cs.numeric().rank(method)) / (
-        len(data) + 1
-    )  # we normalise so that it is between 0 and 1
+def _compute_cdf_and_pobs(
+    data: pl.DataFrame, marginal_name: str, prob: ProbVector
+) -> pl.DataFrame:
+    norm_const = data.height / (data.height + 1)
+
+    return (
+        data.select(pl.col(marginal_name))
+        .with_row_index()
+        .with_columns(prob=prob)
+        .sort(marginal_name)
+        .with_columns(
+            cdf=pl.cum_sum("prob") * norm_const,
+        )
+        .with_columns(
+            pobs=pl.col("cdf").gather(pl.col("index").arg_sort()),
+        )
+    )
 
 
-def emp_cdf(data: pl.DataFrame, method: RankMethod = "average") -> pl.DataFrame:
-    return data.select(cs.numeric().rank(method, descending=True))
+def cma_separation(data: pl.DataFrame, prob: ProbVector):
+    cdf_cols = {}
+    copula_cols = {}
+
+    for col in data.iter_columns():
+        name = col.name
+        temp = _compute_cdf_and_pobs(data, name, prob)
+        cdf_cols[f"{name}_cdf"] = temp["cdf"]
+        copula_cols[f"{name}_pobs"] = temp["pobs"]
+
+    cdf = pl.DataFrame(cdf_cols)
+    copula = pl.DataFrame(copula_cols)
+
+    return data, cdf, copula
