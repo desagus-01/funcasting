00d0f32ceab2a7407e9304ccf96a03f7fbbe1785
desagus <gugafera2016@outlook.com>
2025-11-11 13:57:37 +0000

    adding example with iteration

--- notebooks/cma_test.py
+++ notebooks/cma_test.py
@@ -25,4 +25,34 @@ test = (
 
 
 multi_df = info["increms_df"]
-print(multi_df)
+
+cdf_cols = {}
+copula_cols = {}
+
+for col in multi_df.iter_columns():
+    name = col.name
+    df = pl.DataFrame({name: col})
+    n = df.height
+    norm_const = n / (n + 1)
+    prob = uniform_probs(n)
+
+    temp = (
+        df.with_row_index()
+        .with_columns(prob=prob)
+        .sort(name)
+        .with_columns(
+            cdf=pl.cum_sum("prob") * norm_const,
+        )
+        .with_columns(
+            pobs=pl.col("cdf").gather(pl.col("index").arg_sort()),
+        )
+    )
+
+    # you said you’ll just pluck pobs, so do that
+    cdf_cols[name] = temp["cdf"]
+    copula_cols[name] = temp["pobs"]
+
+cdf = pl.DataFrame(cdf_cols)
+copula = pl.DataFrame(copula_cols)
+
+print(copula)
