4cfb2a1e8a5d43f10f354fc0d1f480cdb8892ce8
desagus <gugafera2016@outlook.com>
2025-12-01 10:58:12 +0000

    adding basic sw imp

--- notebooks/invariances.py
+++ notebooks/invariances.py
@@ -5,7 +5,7 @@ from utils.template import get_template
 info_all = get_template()
 ts_aapl = info_all.increms_df.select(["date", "AAPL"])
 
-ts_2 = ts_aapl.select("date", "AAPL", lag_aapl=pl.col("AAPL").shift())
+ts_2 = ts_aapl.select("date", "AAPL", lag_aapl=pl.col("AAPL").shift(1))
 
 
 print(ts_2)
