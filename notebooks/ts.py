0c427f1aea5b3e2adc35609d372c8f4b33f1d94d
desagus <gugafera2016@outlook.com>
2025-12-12 13:49:52 +0000

    slight updated to cutoffs

--- notebooks/ts.py
+++ notebooks/ts.py
@@ -1,5 +1,5 @@
 # %% imports
-
+from globals import MACKIN_TAU_CUTOFFS
 from maths.time_series import adf_max_lag, build_adf_equation, ols
 from utils.template import get_template
 
@@ -37,4 +37,23 @@ used_lag = max_lags
 
 res = ols(adf_eq.dep_vars, adf_eq.ind_var)
 
-res[2].shape
+MACKIN_TAU_CUTOFFS.keys()
+
+
+# %%
+# def mackinnonp(teststat, regression="c", N=1, lags=None):
+#     min_cutoff = MACKIN_TAU_CUTOFFS[f"min_{regression}"]
+#     max_cutoff = MACKIN_TAU_CUTOFFS[f"max_{regression}"]
+#     starstat = MACKIN_TAU_CUTOFFS[f"star_{regression}"]
+#
+#     if teststat > max_cutoff[N - 1]:
+#         return 1.0
+#     elif teststat < min_cutoff[N - 1]:
+#         return 0.0
+#     if teststat <= starstat[N - 1]:
+#         tau_coef = eval("tau_" + regression + "_smallp[" + str(N - 1) + "]")
+#     #        teststat = np.log(np.abs(teststat))
+#     # above is only for z stats
+#     else:
+#         tau_coef = eval("tau_" + regression + "_largep[" + str(N - 1) + "]")
+#     return norm.cdf(polyval(tau_coef[::-1], teststat))
