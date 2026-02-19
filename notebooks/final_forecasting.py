969ffdcfaa614c96ec47ec04c8ccf3508725e552
Gustavo de Sa <gugafera2016@outlook.com>
2026-02-19 17:45:47 +0000

    minor change

--- notebooks/final_forecasting.py
+++ notebooks/final_forecasting.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 from maths.distributions import uniform_probs
 from methods.forecasting_pipeline import run_n_steps_forecast
 from utils.template import get_template, synthetic_series
+from utils.visuals import plot_simulation_results
 
 # %%
 
@@ -10,12 +13,21 @@ data = data.with_columns(fake=series)
 
 # %%
 prob_ex = uniform_probs(data.height)
-run_n_steps_forecast(
+forecasts = run_n_steps_forecast(
     data=data,
     prob=prob_ex,
     horizon=100,
     n_sims=1000,
     seed=1,
     assets=["AAPL", "GOOG", "MSFT", "fake"],
-    method="bootstrap",
+    method="cma",
+    target_copula="t",
 )
+
+# %%
+
+for asset, res in forecasts.items():
+    logP_paths = 0.0 + np.cumsum(res, axis=1)
+    paths = np.exp(logP_paths)
+
+    plot_simulation_results(paths, title=f"{asset}")
