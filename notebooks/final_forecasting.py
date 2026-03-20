007a703d1501478dad86bc3ca9d231784c9a40d7
desagus <gugafera2016@outlook.com>
2026-03-20 17:05:07 +0000

    minor change

--- notebooks/final_forecasting.py
+++ notebooks/final_forecasting.py
@@ -3,6 +3,7 @@ import polars as pl
 
 from maths.distributions import uniform_probs
 from methods.forecasting_pipeline import run_n_steps_forecast
+from methods.preprocess_pipeline import run_univariate_preprocess
 from utils.tiingo import plot_ticker_lines
 from utils.visuals import plot_simulation_results
 
@@ -10,11 +11,12 @@ from utils.visuals import plot_simulation_results
 data = pl.read_csv("./data/tiingo_sample.csv")
 # assets = ["SMBC", "RDN", "BANC", "FCN"]
 
-assets = ["BANC"]
+assets = ["SMBC", "RDN", "BANC", "FCN"]
 data = data.select("date", *assets)
 
 # %%
 data
+data.select(pl.col("BANC").exp())
 
 # %%
 data_long = (
@@ -43,6 +45,26 @@ forecasts = run_n_steps_forecast(
     target_copula="t",
 )
 
+
+# %%
+asset = "BANC"
+
+post_process = run_univariate_preprocess(data=data, assets=assets)
+
+orig = data.select(asset).to_numpy().ravel()
+post = post_process.post_data.select(asset).to_numpy().ravel()
+
+c = orig.mean() - post.mean()
+
+res = forecasts[asset]
+logP_paths = res + c
+paths = np.exp(logP_paths)
+
+plot_simulation_results(paths, title=f"{asset}")
+print("c:", c)
+print("res sample:", res[0, :5])
+print("logP sample:", logP_paths[0, :5])
+print("price sample:", paths[0, :5])
 
 # %%
 for asset, res in forecasts.items():
